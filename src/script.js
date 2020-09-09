412c846bc9c1a65c31904b210f685c2b4c08d369
Ali Spittel <ali07cat07@gmail.com>
2020-09-09 15:27:32 -0500

    add code

---
+++ src/script.js
@@ -0,0 +1,91 @@
+import Amplify, { API, graphqlOperation } from '@aws-amplify/api'
+
+import awsConfig from '../aws-exports'
+import { createGif, deleteGif, updateGif } from './graphql/mutations'
+import { listGifs } from './graphql/queries'
+
+Amplify.configure(awsConfig)
+
+let currentGifId = ''
+
+const editGif = async () => {
+  try {
+    return await API.graphql(graphqlOperation(updateGif, {
+      input: {
+        id: currentGifId,
+        name: document.getElementById('edit-name').value,
+        url: document.getElementById('edit-url').value
+      }
+    }))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const removeGif = async () => {
+  await API.graphql(graphqlOperation(deleteGif, {
+    input: { id: currentGifId }
+  }))
+}
+
+const populateModal = gif => {
+  currentGifId = gif.id
+  document.getElementById('edit-modal').classList.remove('hidden')
+  document.getElementById('edit-name').value = gif.name
+  document.getElementById('edit-url').value = gif.url
+}
+
+const createGifUi = gif => {
+  const img = document.createElement('img')
+  img.setAttribute('src', gif.url)
+  img.addEventListener('click', () => populateModal(gif))
+  document.querySelector('.container').appendChild(img)
+}
+
+const getGifs = async () => {
+  const container = document.querySelector('.container')
+  container.innerHTML = ''
+  const gifs = await API.graphql(graphqlOperation(listGifs))
+  gifs.data.listGifs.items.map(createGifUi)
+}
+
+const createNewGif = async e => {
+  e.preventDefault()
+
+  const gif = {
+    name: document.getElementById('name').value,
+    url: document.getElementById('url').value
+  }
+
+  try {
+    await API.graphql(graphqlOperation(createGif, { input: gif }))
+    document.getElementById('create-form').reset()
+    getGifs()
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const closeModal = () => document.getElementById('edit-modal').classList.add('hidden')
+const closeCreateModal = () => document.getElementById('create-modal').classList.add('hidden')
+
+document.getElementById('close-edit-button').addEventListener('click', closeModal)
+document.getElementById('close-create-button').addEventListener('click', closeCreateModal)
+
+document.getElementById('create-form').addEventListener('submit', createNewGif)
+document.getElementById('edit-form').addEventListener('submit', e => {
+  e.preventDefault()
+  editGif()
+  getGifs()
+  closeModal()
+})
+
+document.getElementById('delete-button').addEventListener('click', () => {
+  removeGif()
+  getGifs()
+  closeModal()
+})
+
+document.getElementById('plus-button').addEventListener('click', () => document.getElementById('create-modal').classList.remove('hidden'))
+
+getGifs()
